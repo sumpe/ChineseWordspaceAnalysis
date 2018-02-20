9c63082653b2d5f99fad71b33879f44b10cada1e
Peter Sumbler <peter@gavagai.se>
2018-02-20 14:25:27 +0100

    Terminology changes.

--- n_gram_consumption.py
+++ n_gram_consumption.py
@@ -8,11 +8,12 @@ def compare_bad_space_with_good_space(good_space, bad_space):
     total = len(good_vocabulary)
     overlapping = list_intersection_size(good_vocabulary, bad_vocabulary)
     new_words = [word for word in bad_vocabulary if word not in good_vocabulary]
+    dropped_out_words = [word for word in good_vocabulary if word not in bad_vocabulary]
     print('start to count')
     count = 0
-    for good_word in good_vocabulary:
+    for dropped_word in dropped_out_words:
         for new_word in new_words:
-            if good_word in new_word:
+            if dropped_word in new_word:
                 count += 1
                 break
     print(overlapping, overlapping/total)
