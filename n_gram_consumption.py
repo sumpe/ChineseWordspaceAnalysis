7af1d43295738b68e2f2ec8decd0cc50758149df
Peter Sumbler <peter@gavagai.se>
2018-02-21 15:50:32 +0100

    Comparison with old word space entirely.

--- n_gram_consumption.py
+++ n_gram_consumption.py
@@ -5,6 +5,7 @@ from time import time
 def compare_bad_space_with_good_space(good_space, bad_space):
     good_vocabulary = extract_vocab_from_directory(good_space)
     bad_vocabulary = extract_vocab_from_directory(bad_space)
+    previous_full_space = extract_vocab_from_directory('wordspaces/2017-12-12')
     total = len(good_vocabulary)
     overlapping = list_intersection_size(good_vocabulary, bad_vocabulary)
     new_words = [word for word in bad_vocabulary if word not in good_vocabulary]
@@ -12,15 +13,16 @@ def compare_bad_space_with_good_space(good_space, bad_space):
     count = 0
     for good_word in good_vocabulary:
         for new_word in new_words:
-            if good_word in new_word:
-                count += 1
-                break
+            if new_word not in previous_full_space:
+                if good_word in new_word:
+                    count += 1
+                    break
     print(overlapping, overlapping/total)
     print(count, count/total)
 
 
 start = time()
-compare_bad_space_with_good_space('lexicon/2017-12-12', 'lexicon/2018-02-14')
+compare_bad_space_with_good_space('lexicon/2017-12-12', 'wordspaces/2018-02-14')
 end = time()
 
 print(end-start)
